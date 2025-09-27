08c28d017158a10cb98167548762af23c0e977d0
Yasir <139705368+yasir54443@users.noreply.github.com>
2025-09-26 23:12:08 -0700

    Delete Main.js

--- Main.js
+++
@@ -1,5 +0,0 @@
-// Toggle mobile menu
-document.getElementById("menu-toggle").addEventListener("click", () => {
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("show");
-});
