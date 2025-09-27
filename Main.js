81c1aa657d8af0c6cc31828a74dd39c97e61867c
Yasir <139705368+yasir54443@users.noreply.github.com>
2025-09-26 23:06:36 -0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,5 @@
+// Toggle mobile menu
+document.getElementById("menu-toggle").addEventListener("click", () => {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show");
+});
